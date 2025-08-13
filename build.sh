4aa60405677f4b70a7c72ed85d34e2e4c00dce7f
BryndCraft <bryndcraf@gmail.com>
2025-08-12 19:43:37 -0600

    cambio

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Convertir las traducciones
-python manage.py compilemessages
-
-# Aplicar migraciones
 python manage.py migrate
-
-# Recopilar archivos estáticos
 python manage.py collectstatic --no-input
